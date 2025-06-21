94f6d89722aa5a940222ddadbb577ca4ede6377f
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-06-21 16:22:48 +0400

    added logging with loguru + updated port value

--- fraud_detection/predict.py
+++ fraud_detection/predict.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from loguru import logger
 import mlflow
 import mlflow.sklearn
 import pandas as pd
@@ -7,14 +8,29 @@ from fraud_detection.common import get_X
 
 app = FastAPI()
 
-mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_tracking_uri("http://mlflow:5000")
+
+logger.add(
+    "logs/predict.log",
+    rotation="1 MB",
+    retention="10 days",
+    level="INFO",
+    format="{time} {level} {message}",
+)
 
 
 def load_model():
     model_name = "trained_rfc"
-    model = mlflow.sklearn.load_model(
-        model_uri=f"models:/{model_name}/latest",
-    )
+    try:
+        logger.info("Connecting to MLflow tracking server at http://mlflow:5000")
+
+        model = mlflow.sklearn.load_model(
+            model_uri=f"models:/{model_name}/latest",
+        )
+        logger.info("Model loaded from MLflow: {}", model_name)
+    except Exception:
+        logger.error("Failed to load model. Ensure that the model is registered in MLflow.")
+        return None
 
     return model
 
@@ -26,15 +42,25 @@ def root():
 
 @app.post("/predict")
 def predict(data: dict):
+    logger.info("Received data for prediction: {}", data)
+
     data = pd.DataFrame(data)
+    logger.info("Data converted to DataFrame:\n{}", data)
+
     X = get_X(data=data)
+    logger.info("Did something to X:\n{}", X)
+
     model = load_model()
+    logger.info("Model loaded successfully.")
 
     prediction = model.predict(X)[0]
+    logger.info("Prediction made: {}", prediction)
 
     if prediction == 1:
         prediction = "Fraud"
     else:
         prediction = "Not Fraud"
+
+    logger.info("Final prediction: {}", prediction)
 
     return {"prediction": prediction}
